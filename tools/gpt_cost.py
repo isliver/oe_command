721ccc0741e53052831532d616c62263c36976a8
Esteban <isliver31415@gmail.com>
2024-04-10 11:40:04 -0400

    feat: update cost log

--- tools/gpt_cost.py
+++ tools/gpt_cost.py
@@ -1,68 +1,76 @@
 import os
+import json
+from datetime import datetime
+from constant import prices
 
-globalCompletionTokens = 0
-globalPromptTokens = 0
-globalTotalTokens = 0
 pathHome = os.environ['HOME']
 pathLogCost = f"{pathHome}/.config/oe"
 
-def saveCost (cost):
+def saveCost (model, cost):
     if not os.path.exists(pathLogCost):
         os.makedirs(pathLogCost)
 
-    f = open(f"{pathLogCost}/log_cost", "a")
-    f.write(str(cost) + "\n")
-    f.close()
-
-def showTotalCost ():
-    f = open(f"{pathLogCost}/log_cost", "r")
-    totalCost = 0
-
-    for x in f:
-        totalCost += float(x)
+    now = datetime.utcnow()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    cost["ts"] = timestamp
     
-    print(f"Total cost: ${totalCost}")
+    with open(f"{pathLogCost}/log_cost.json", 'r') as file:
+        logData = json.load(file)
 
-def addResponse (response, verbose=False):
-    global globalCompletionTokens
-    global globalPromptTokens
-    global globalTotalTokens
+    if model not in logData:
+        logData[model] = []
 
+    logData[model].append(cost)
+
+    with open(f"{pathLogCost}/log_cost.json", 'w') as file:
+        json.dump(logData, file, indent=4)
+
+def openLogCost ():
+    with open(f"{pathLogCost}/log_cost.json", 'r') as file:
+        logData = json.load(file)
+    
+    return logData
+
+def addResponse (response, model, verbose=False):
     completionTokens = 0
 
     if 'completion_tokens' in response['usage']:
         completionTokens = response['usage']['completion_tokens']
+    else:
+        return ''
+    
+    pricesModel = prices.prices[model]
 
     promptTokens = response['usage']['prompt_tokens']
     totalTokens = response['usage']['total_tokens']
-    
-    globalCompletionTokens += completionTokens
-    globalPromptTokens += promptTokens
-    globalTotalTokens += totalTokens
+
+    promptCosts = promptTokens * pricesModel["input"]
+    completionCosts = completionTokens * pricesModel["output"]
+
+    cost = {
+        "prompt_cost": promptCosts / pricesModel["numerTokens"],
+        "completion_cost": completionCosts / pricesModel["numerTokens"]
+    }
+
+    saveCost(model, cost)
 
     if verbose:
         print(f"Tokens resume = completion: {completionTokens} prompt: {promptTokens} total: {totalTokens}")
     
     return f"Tokens resume = completion: {completionTokens} prompt: {promptTokens} total: {totalTokens}"
 
-def showCost (model="text-davinci-003", save=False, verbose=False):
-    global globalCompletionTokens
-    global globalPromptTokens
-    global globalTotalTokens
-    
-    costByTokens = costByModel(model)
-    finalCost = ( globalTotalTokens * costByTokens ) / 1000
-    saveCost (finalCost)
+def showCost ():
+    costs = openLogCost()
 
-    if verbose:
-        print(f"MODEL: {model}")
-        print(f"Final Tokens consume = completion: {globalCompletionTokens} prompt: {globalPromptTokens} total: {globalTotalTokens}")
-        print(f"Final cost: ${finalCost}")
-    
-    return f"Final Tokens consume = completion: {globalCompletionTokens} prompt: {globalPromptTokens} total: {globalTotalTokens}"
+    for model, values in costs.items():
+        print(f"* Costos {model}")
 
-def costByModel (model):
-    if model == "text-davinci-003":
-        return 0.02
-    elif model == "gpt-3.5-turbo":
-        return 0.002
+        prompt_costs = 0
+        completion_costs = 0
+
+        for obj in values:
+            prompt_costs += obj["prompt_cost"]
+            completion_costs += obj["completion_cost"]
+        
+        print(f"Prompt: ${prompt_costs}")
+        print(f"Completion: ${completion_costs}")
