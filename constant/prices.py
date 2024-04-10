68451a11e5c29ba1bd7f6cf988416a62b9792149
Esteban <isliver31415@gmail.com>
2024-04-10 11:44:32 -0400

    refactor: add prices

---
+++ constant/prices.py
@@ -0,0 +1,22 @@
+prices = {
+    "gpt-3.5-turbo": {
+        "input": 0.50,
+        "output": 1.50,
+        "numerTokens": 1_000_000
+    },
+    "gpt-3.5-turbo-instruct": {
+        "input": 1.50,
+        "output": 2.00,
+        "numerTokens": 1_000_000
+    },
+    "gpt-4": {
+        "input": 30.00,
+        "output": 60.00,
+        "numerTokens": 1_000_000
+    },
+    "gpt-4-turbo": {
+        "input": 60.00,
+        "output": 120.00,
+        "numerTokens": 1_000_000
+    }
+}
