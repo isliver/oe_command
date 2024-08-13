2c8176329883e8e308950d40067909db29e7f96d
Esteban <isliver31415@gmail.com>
2024-08-13 15:14:06 -0400

    feat: add new cost models

--- constant/prices.py
+++ constant/prices.py
@@ -15,8 +15,18 @@ prices = {
         "numerTokens": 1_000_000
     },
     "gpt-4-turbo": {
-        "input": 60.00,
-        "output": 120.00,
+        "input": 10.00,
+        "output": 30.00,
+        "numerTokens": 1_000_000
+    },
+    "gpt-4o": {
+        "input": 5.00,
+        "output": 15.00,
+        "numerTokens": 1_000_000
+    },
+    "gpt-4o-mini": {
+        "input": 0.150,
+        "output": 0.6,
         "numerTokens": 1_000_000
     }
 }
